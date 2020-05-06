744b33216266f1830adaafab2b1e01cef23cb61e
shivam <shivamsoni.soni3@gmail.com>
2020-05-06 12:01:58 +0530

    "Image assest folder added & images added for recursion"

--- Recursion/introrec.cpp
+++ Recursion/introrec.cpp
@@ -2,32 +2,33 @@
 
 using std ::cout;
 
-void fun_type1(int a)
+void calling(int a)
 {
-    // this fun will print the number in the desending order because it is sending output to the console at the time of calling
-    
-    if (a > 0)
-    {
-        cout << a;
-        fun_type1(a - 1);
-    }
-}
-
-void fun_type2(int a)
-{ 
-    // this fun will print the number in the assending order because it is sending output to the console at the time of returning.
+    // this fun will print the number in the desending order because it is sending output to the console at the time of calling or before calling
 
     if (a > 0)
     {
-        fun_type2(a - 1);
+        cout << a;
+        calling(a - 1);
+    }
+}
+
+void returning(int a)
+{
+    // this fun will print the number in the assending order because it is sending output to the console at the time of returning i.e after calling.
+
+    if (a > 0)
+    {
+        returning(a - 1);
         cout << a;
     }
-    
 }
+
+
 int main()
 {
-    fun_type1(3);
-    fun_type2(3);
+    calling(3);
+    returning(3);
 
     return 0;
 }
--- Recursion/notes_r.md
+++ Recursion/notes_r.md
@@ -1,11 +1,14 @@
 
 # NOTES RECURSION
 
-recursive function is a function which is calling itself.
+recursive function is a function which  calls itself (So a function inside function).
 So if a function is calling itself then it is Scalise a recursive function.
-So a function inside that if you see it is calling itself again.
-It is just like this First we call the function first time then it will call itself again call itself.
-again it called itself so repeatedly it will be calling again and again.
-So there must be some condition at which it must stop.
+the funtion calls itself repeatatively  so it will be forming an infinite loop so to terminate the function there always should a condition which terminates the function.
+There are two phases in recursion
 
-One is a calling phase other one is returning phase or I can't say that this was ascending and then
+- One is a calling phase
+- other one is returning phase
+
+# How Recursion uses stack
+
+![Recursion Stack Use]()
